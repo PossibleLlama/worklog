ac8d4d4d34768d282b748a5b44ba46bfa8a1fd61
Chris Collier <chris.collier@kaluza.com>
2020-10-02 13:39:26 +0100

    Add where the work recrods should sit, to metadata

--- src/metadata.go
+++ src/metadata.go
@@ -9,21 +9,23 @@ import (
 )
 
 const (
-	fileName string = "/.worklog.yml"
+	configFileName	string = "config.yml"
 )
 
 // MetadataFile information added to each worklog
 type MetadataFile struct {
-	Author string `yaml:"author"`
+	Author			string `yaml:"author"`
+	RecordLocation	string `yaml:"recordLocation"`
 }
 
 func getMetadata(metadataChan chan<- MetadataFile) {
 	var file MetadataFile
-	filePath := os.Getenv("HOME") + fileName
+	filePath := os.Getenv("HOME") + "/.worklog/"
+	configFilePath := filePath + configFileName
 
-	data, err := ioutil.ReadFile(filePath)
+	data, err := ioutil.ReadFile(configFilePath)
 	if err != nil {
-		fmt.Printf("cannot open file: %s\n", filePath)
+		fmt.Printf("cannot open file: %s\n", configFilePath)
 		metadataChan <- MetadataFile{}
 	}
 
@@ -34,8 +36,11 @@ func getMetadata(metadataChan chan<- MetadataFile) {
 	}
 
 	if file.Author == "" {
-		fmt.Printf("unable to get 'author' property from ~%s file.\n", fileName)
+		fmt.Printf("unable to get 'author' property from ~%s file.\n", configFileName)
 		os.Exit(1)
+	}
+	if file.RecordLocation == "" {
+		file.RecordLocation = filePath
 	}
 
 	metadataChan <- file
