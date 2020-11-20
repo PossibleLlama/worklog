618dccd72af609b02eedf7a0aba286f28e359053
Chris Collier <chris.collier@kaluza.com>
2020-11-20 08:56:22 +0000

    014 Check date with regex instead of by length

--- src/helpers/dates.go
+++ src/helpers/dates.go
@@ -2,8 +2,17 @@ package helpers
 
 import (
 	"fmt"
+	"regexp"
 	"time"
 )
+
+// These regex'es are not expected to be exhaustive.
+// They will be used to check whether this is likely
+// to be a date, and not a random string
+const dateRegex = `[0-9]{4}[-/ ][0-1][0-9][-/ ][0-3][0-9]`
+const timeRegex = `[0-2][0-9]:[0-5][0-9]:[0-5][0-9]`
+const dateTimeRegex = dateRegex + `[\sT]` + timeRegex
+const dateTimeRFC3339Regex = dateTimeRegex + `Z`
 
 // TimeFormat formats a time to string
 func TimeFormat(t time.Time) string {
@@ -13,12 +22,21 @@ func TimeFormat(t time.Time) string {
 // GetStringAsDateTime ensures a string is a dateTime
 func GetStringAsDateTime(element string) (time.Time, error) {
 	var dateString string
-	if len(element) == 10 {
-		dateString = fmt.Sprintf("%sT00:00:00Z", element)
-	} else if len(element) == 19 {
+
+	_, dateErr := regexp.MatchString(dateRegex, element)
+	isDateTime, dateTimeErr := regexp.MatchString(dateTimeRegex, element)
+	isRFC339, rfc399Err := regexp.MatchString(dateTimeRFC3339Regex, element)
+
+	if dateErr != nil || dateTimeErr != nil || rfc399Err != nil {
+		return time.Now(), fmt.Errorf("unable to parse string as date")
+	}
+
+	if isRFC339 {
+		dateString = element
+	} else if isDateTime {
 		dateString = fmt.Sprintf("%sZ", element)
 	} else {
-		dateString = element
+		dateString = fmt.Sprintf("%sT00:00:00Z", element)
 	}
 	date, err := time.Parse(time.RFC3339, dateString)
 	if err != nil {
