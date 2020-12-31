759e99c5c56270bf8a73020c4c2d112488d131d2
chris.collier <christopher.collier@zoopla.co.uk>
2020-12-31 10:42:05 +0000

    046 Add mock repo for testing

---
+++ repository/mockRepo.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"time"
+
+	"github.com/PossibleLlama/worklog/model"
+	"github.com/stretchr/testify/mock"
+)
+
+// MockRepo type of WorklogRepository for testing
+type MockRepo struct {
+	mock.Mock
+}
+
+// Configure WorklogRepository method for testing
+func (m *MockRepo) Configure(cfg *model.Config) error {
+	args := m.Called(cfg)
+	return args.Error(0)
+}
+
+// Save WorklogRepository method for testing
+func (m *MockRepo) Save(wl *model.Work) error {
+	args := m.Called(wl)
+	return args.Error(0)
+}
+
+// GetAllBetweenDates WorklogRepository method for testing
+func (m *MockRepo) GetAllBetweenDates(startDate, endDate time.Time, filter *model.Work) ([]*model.Work, error) {
+	args := m.Called(startDate, endDate, filter)
+	return []*model.Work{}, args.Error(0)
+}
