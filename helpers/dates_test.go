4de529617bc338045c003fe81d46f2b5242703a9
chris.collier <christopher.collier@zoopla.co.uk>
2020-12-31 13:37:38 +0000

    052 Update time format tests to cover each of the provided formats

--- helpers/dates_test.go
+++ helpers/dates_test.go
@@ -7,11 +7,26 @@ import (
 	"time"
 )
 
-func TestTimeFormat(t *testing.T) {
-	parsedTime, err := time.Parse(time.RFC3339, "2000-01-02T01:23:45Z")
+const (
+	outputTime = "2000-01-02T01:23:00Z"
+)
+
+func initilizeTime(t *testing.T, layout, value string) time.Time {
+	tm, err := time.Parse(layout, value)
 	if err != nil {
 		t.Errorf("Initialization of test data failed with %s", err)
 	}
+	return tm
+}
+
+func TestTimeFormat(t *testing.T) {
+	rfc3339Time := initilizeTime(t, time.RFC3339, "2000-01-02T01:23:00Z")
+	rfc3339NanoTime := initilizeTime(t, time.RFC3339Nano, "2000-01-02T01:23:00.000000000Z")
+	rfc1123Time := initilizeTime(t, time.RFC1123, "Mon, 02 Jan 2000 01:23:00 GMT")
+	rfc1123ZTime := initilizeTime(t, time.RFC1123Z, "Mon, 02 Jan 2000 01:23:00 +0000")
+	rfc822Time := initilizeTime(t, time.RFC822, "02 Jan 00 01:23 GMT")
+	rfc822ZTime := initilizeTime(t, time.RFC822Z, "02 Jan 00 01:23 +0000")
+	rfc850Time := initilizeTime(t, time.RFC850, "Monday, 02-Jan-00 01:23:00 GMT")
 
 	var tests = []struct {
 		name   string
@@ -20,8 +35,32 @@ func TestTimeFormat(t *testing.T) {
 	}{
 		{
 			name:   "Valid RFC3339 date time is same",
-			input:  parsedTime,
-			output: "2000-01-02T01:23:45Z",
+			input:  rfc3339Time,
+			output: outputTime,
+		}, {
+			name:   "Valid RFC3339 nano date time updates",
+			input:  rfc3339NanoTime,
+			output: outputTime,
+		}, {
+			name:   "Valid RFC1123 date time updates",
+			input:  rfc1123Time,
+			output: outputTime,
+		}, {
+			name:   "Valid RFC1123Z date time updates",
+			input:  rfc1123ZTime,
+			output: outputTime,
+		}, {
+			name:   "Valid RFC882 date time updates",
+			input:  rfc822Time,
+			output: outputTime,
+		}, {
+			name:   "Valid RFC882Z date time updates",
+			input:  rfc822ZTime,
+			output: outputTime,
+		}, {
+			name:   "Valid RFC850 date time updates",
+			input:  rfc850Time,
+			output: outputTime,
 		},
 	}
 
