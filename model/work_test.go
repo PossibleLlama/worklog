ee5007a44c15b540cd641de77e8616229ea80820
chris.collier <christopher.collier@zoopla.co.uk>
2021-01-04 09:02:52 +0000

    050 Use an assert instead of lots of if's

--- model/work_test.go
+++ model/work_test.go
@@ -3,6 +3,8 @@ package model
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewWork(t *testing.T) {
@@ -61,7 +63,6 @@ func TestNewWork(t *testing.T) {
 
 	for _, testItem := range tests {
 		t.Run(testItem.name, func(t *testing.T) {
-			before := time.Now()
 			actual := NewWork(
 				testItem.wTitle,
 				testItem.wDescription,
@@ -69,41 +70,13 @@ func TestNewWork(t *testing.T) {
 				testItem.wDuration,
 				testItem.wTags,
 				testItem.wWhen)
-			after := time.Now()
+			finished := time.Now()
 
-			if actual.Title != testItem.expected.Title {
-				t.Errorf("Should have title %s, instead has %s", testItem.expected.Title, actual.Title)
-			}
-			if actual.Description != testItem.expected.Description {
-				t.Errorf("Should have description %s, instead has %s", testItem.expected.Description, actual.Description)
-			}
-			if actual.Author != testItem.expected.Author {
-				t.Errorf("Should have author %s, instead has %s", testItem.expected.Author, actual.Author)
-			}
-			if actual.Where != testItem.expected.Where {
-				t.Errorf("Should have where %s, instead has %s", testItem.expected.Where, actual.Where)
-			}
-			if actual.Duration != testItem.expected.Duration {
-				t.Errorf("Should have duration %d, instead has %d", testItem.expected.Duration, actual.Duration)
-			}
-			if len(actual.Tags) != len(testItem.expected.Tags) {
-				t.Errorf("Should have same number of tags %d, instead has %d", len(testItem.expected.Tags), len(actual.Tags))
-			}
-			for index, element := range testItem.expected.Tags {
-				if actual.Tags[index] != element {
-					t.Errorf("Item %s does not match element %s in list %s",
-						element, actual.Tags[index], actual.Tags)
-				}
-			}
-			if !actual.When.Equal(testItem.expected.When) {
-				t.Errorf("Should have when '%s', instead has '%s'", actual.When, actual.CreatedAt)
-			}
-			if time.Since(actual.CreatedAt) < time.Since(before) {
-				t.Error("Was not created after start of test")
-			}
-			if time.Since(actual.CreatedAt) < time.Since(after) {
-				t.Error("Was not created before end of test")
-			}
+			// Instead of mocking time.Now(), just set the result of it to the expected value
+			testItem.expected.CreatedAt = actual.CreatedAt
+
+			assert.Equal(t, testItem.expected, actual)
+			assert.True(t, finished.Add(time.Second*-1).Before(actual.CreatedAt))
 		})
 	}
 }
