d55c1f2b1bda6fc7e78350d691b07da6977410db
Chris Collier <possiblellama@gmail.com>
2021-01-17 16:34:58 +0000

    048 Initial creation of tests
    Running of configuration.
    
    Makes changes to the ~/.worklog/config file.

---
+++ integtest/configure_test.go
@@ -0,0 +1,47 @@
+package integtest
+
+import (
+	"os"
+	"os/exec"
+	"path"
+	"testing"
+
+	"github.com/PossibleLlama/worklog/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigure(t *testing.T) {
+	var tests = []struct {
+		name      string
+		args      []string
+		expOutput string
+		expFile   model.Config
+	}{
+		{
+			name:      "defaults are used",
+			args:      []string{},
+			expOutput: "Successfully configured\n",
+			expFile:   model.Config{},
+		},
+	}
+
+	for _, testItem := range tests {
+		t.Run(testItem.name, func(t *testing.T) {
+			dir, err := os.Getwd()
+			if err != nil {
+				t.Error(err)
+			}
+
+			testItem.args = append([]string{"configure"}, testItem.args...)
+			cmd := exec.Command(path.Join(dir, binaryName), testItem.args...)
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Error(err)
+			}
+
+			actual := string(output)
+
+			assert.Equal(t, testItem.expOutput, actual)
+		})
+	}
+}
---
+++ integtest/helpers.go
@@ -0,0 +1,3 @@
+package integtest
+
+const binaryName = "worklog"
