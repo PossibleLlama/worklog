30814508f2c0ad5c175d56e54614a3f8bf02b3ef
Chris Collier <possiblellama@gmail.com>
2021-01-17 17:22:02 +0000

    048 Add test for override with no argument
    To allow this, needed to change expected file to optional.
    
    If expectedFile was not optional, it would make orders of tests matter.
    
    Also assert on the returned error from running worklog.

--- integtest/configure_test.go
+++ integtest/configure_test.go
@@ -10,24 +10,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const overrideNoArgs = `Error: defaults requires at least one argument
+Usage:
+  worklog configure overrideDefaults [flags]
+
+Flags:
+      --author string   The authour for all work
+      --duration int    Default duration that work takes (default -1)
+      --format string   Format to print work in. If provided, must be one of 'pretty', 'yaml', 'json'
+  -h, --help            help for overrideDefaults
+
+Global Flags:
+      --config string   config file (default is $HOME/.worklog/config.yml)
+
+defaults requires at least one argument
+`
+
 func TestConfigure(t *testing.T) {
 	var tests = []struct {
 		name      string
 		args      []string
+		success   bool
 		expOutput string
-		expFile   model.Config
+		expFile   *model.Config
 	}{
 		{
 			name:      "defaults are used",
 			args:      []string{},
+			success:   true,
 			expOutput: "Successfully configured\n",
-			expFile: model.Config{
+			expFile: &model.Config{
 				Author: "",
 				Defaults: model.Defaults{
 					Duration: 15,
 					Format:   "pretty",
 				},
 			},
+		}, {
+			name:      "override defaults requires an argument",
+			args:      []string{"overrideDefaults"},
+			success:   false,
+			expOutput: overrideNoArgs,
+			expFile:   nil,
 		},
 	}
 
@@ -41,12 +65,14 @@ func TestConfigure(t *testing.T) {
 			testItem.args = append([]string{"configure"}, testItem.args...)
 			cmd := exec.Command(path.Join(dir, binaryName), testItem.args...)
 			output, err := cmd.CombinedOutput()
-			if err != nil {
-				t.Error(err)
-			}
 
 			assert.Equal(t, testItem.expOutput, string(output))
-			assert.Equal(t, testItem.expFile, *getActualConfig(t))
+			if testItem.success {
+				assert.Nil(t, err)
+				assert.Equal(t, testItem.expFile, getActualConfig(t))
+			} else {
+				assert.Error(t, err)
+			}
 		})
 	}
 }
