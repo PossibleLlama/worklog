8187fb5214c789dca1ff385e6bd519683b2bca5c
chris.collier <possiblellama@gmail.com>
2021-01-19 13:48:56 +0000

    048 Add into helper, function to only run tests when in first half of second
    Should make integration tests a lot more stable.

--- integration/helpers_test.go
+++ integration/helpers_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"testing"
+	"time"
 
 	"github.com/PossibleLlama/worklog/model"
 	"gopkg.in/yaml.v2"
@@ -14,6 +15,25 @@ import (
 const binaryName = "worklog"
 
 const length = 56
+
+// If we are in the first half of the second.
+// Will hopefully cut out the failures when a second rolls over
+// and causes values to not match.
+func TestMain(m *testing.M) {
+	ran := false
+	for i := 0; i < 10; i++ {
+		now := time.Now()
+		if now.Second() == now.Round(time.Second).Second() {
+			ran = true
+			m.Run()
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+	if !ran {
+		fmt.Print("Didn't pause correctly")
+		m.Run()
+	}
+}
 
 func getActualConfig(t *testing.T) *model.Config {
 	var actualFile model.Config
