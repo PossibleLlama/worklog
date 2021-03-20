cd73f29410066980cf4bd99383f696954e597481
Chris Collier <possiblellama@gmail.com>
2021-03-20 15:42:57 +0000

    067 Integration test for ids

--- integration/print_test.go
+++ integration/print_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/PossibleLlama/worklog/helpers"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -37,6 +38,7 @@ one flag is required
 
 func TestPrint(t *testing.T) {
 	tm := time.Now().Add(time.Hour * length * length * -1)
+	randStr := helpers.RandString(20)
 	var tests = []struct {
 		name      string
 		args      []string
@@ -48,6 +50,16 @@ func TestPrint(t *testing.T) {
 			args:      []string{"--startDate", tm.Format(time.RFC3339), "--endDate", tm.Format(time.RFC3339), "--pretty"},
 			success:   true,
 			expOutput: fmt.Sprintf("No work found between %02d-%02d-%02d 00:00:00 +0000 UTC and %02d-%02d-%02d 23:59:59 +0000 UTC with the given filter\n", tm.Year(), int(tm.Month()), tm.Day(), tm.Year(), int(tm.Month()), tm.Day()),
+		}, {
+			name:      "Print with invalid ID pretty",
+			args:      []string{randStr, "--pretty"},
+			success:   true,
+			expOutput: fmt.Sprintf("No work found between 0001-01-01 00:00:00 +0000 UTC and 0000-12-31 23:59:59 +0000 UTC with the given filter with id's [%s]", randStr),
+		}, {
+			name:      "Print with invalid ID json",
+			args:      []string{randStr, "--json"},
+			success:   true,
+			expOutput: "[]",
 		}, {
 			name:      "No arguments",
 			args:      []string{},
