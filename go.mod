babaa4243fc4d456e928a5a8b9d0c5d0a07282c3
Chris Collier <possiblellama@gmail.com>
2022-04-03 22:05:05 +0000

    057 Remove New Relic aspects
    New Relic logger is expected to be ran on a server, and doesn't send logs/transactons before the CLI has exited.
    
    Therefore the New Relic logger is not suitable for a CLI.

--- go.mod
+++ go.mod
@@ -16,12 +16,10 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
-	github.com/newrelic/go-agent/v3 v3.15.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
@@ -30,11 +28,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20210828152312-66f60bf46e71 // indirect
-	google.golang.org/grpc v1.40.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.63.2 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- helpers/logger.go
+++ helpers/logger.go
@@ -2,54 +2,23 @@ package helpers
 
 import (
 	"fmt"
-	"os"
-	"time"
-
-	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
-var NewRelicApp *newrelic.Application
-
 func init() {
-	app, _ := newrelic.NewApplication(
-		newrelic.ConfigAppName("worklog"),
-		newrelic.ConfigLicense("NEW_RELIC_LICENSE_KEY"),
-		newrelic.ConfigDebugLogger(os.Stdout),
-	)
-	// if err != nil {
-	// 	LogDebug(err.Error(), "startup new relic logger")
-	// }
-	NewRelicApp = app
 }
 
 func LogDebug(msg, event string) {
 	fmt.Printf("%s\n", msg)
-	// logToNewRelic(msg, event, "debug")
 }
 
 func LogInfo(msg, event string) {
 	fmt.Printf("%s\n", msg)
-	logToNewRelic(msg, event, "info")
 }
 
 func LogWarn(msg, event string) {
 	fmt.Printf("%s\n", msg)
-	logToNewRelic(msg, event, "warn")
 }
 
 func LogError(msg, event string) {
 	fmt.Printf("%s\n", msg)
-	logToNewRelic(msg, event, "error")
-}
-
-func logToNewRelic(msg, event, level string) {
-	if NewRelicApp == nil {
-		return
-	}
-	NewRelicApp.RecordCustomEvent(event, map[string]interface{}{
-		"version": Version,
-		"time":    time.Now().UTC().Format(time.RFC3339),
-		"message": msg,
-		"level":   level,
-	})
 }
