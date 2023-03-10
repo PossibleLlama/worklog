930b814b55c065b1cd5d991ca96b48e67d4b390a
Chris Collier <possiblellama@gmail.com>
2023-03-10 08:54:18 +0000

    Ignore issue with defer
    This looks like it could be an issue in limited situations when writing to disk. However I didn't see an example of harm to network connections, and believe this is something a user could replay if there was an issue.

--- server/create.go
+++ server/create.go
@@ -18,6 +18,8 @@ func Create(resp http.ResponseWriter, req *http.Request) {
 		resp.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	// #nosec CWE-703 -- From my understanding, IO errors can occur, which are potentially an issue during file IO.
+	// I haven't seen a similar example of harm to a network IO so will ignore for now.
 	defer req.Body.Close()
 
 	wl := model.NewWork(
